121b639b3b68111263f8cfa9d1549cf7f1725bdc
Ivan <ivanyu327@gmail.com>
2021-12-12 13:41:02 -0500

    11 part 2!

--- advent11/tempCodeRunnerFile.py
+++ advent11/tempCodeRunnerFile.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 
 arr = []
@@ -6,18 +7,17 @@ with open('advent11/info.txt') as f:
     for readline in f:
         arr.append([int(x) for x in readline.strip()])
 
-arr = np.array(arr)
+# arr = np.array(arr)
 
 for line in arr:
     print(line)
 
-step = 0
+flashes = 0
 
-while(True):
-
+for step in range(1,101):
     anotherPass = True
     
-    step+=1
+    print(step)
 
     for r in range(len(arr)):
         for c in range(len(arr[r])):
@@ -33,6 +33,7 @@ while(True):
                 if arr[r][c] > 9:
                     anotherPass = True
                     arr[r][c] = 0
+                    flashes += 1
                     
                     if r-1 >= 0                                     and arr[r-1][c]   != 0:
                         arr[r-1][c]+=1
@@ -53,8 +54,7 @@ while(True):
         
     for line in arr:
         print(line)
-    print(step)
-    if np.count_nonzero(arr==0) == len(arr) * len(arr[0]):
-        break
+        
+                    
 
-print(step)
+print(flashes)
