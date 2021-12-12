cf0d4ffc4d4e8cce6dd696b2c27c5b1459598d32
Ivan <ivanyu327@gmail.com>
2021-12-12 00:02:06 -0500

    cleaned up print statements on 8b

--- advent8/a8b.py
+++ advent8/a8b.py
@@ -8,6 +8,9 @@ with open('advent8/info.txt') as f:
 total = 0
 
 for entry in arr:
+    #entry = arr[0]
+    print(entry)
+
     decoder = [ "" for i in range(10) ]
     signal = entry.split("|")[0].split(" ")
     output = entry.split("|")[1].split(" ")
@@ -15,11 +18,16 @@ for entry in arr:
     signal.pop(signal.index(''))
     output.pop(output.index(''))
 
+    print(decoder)
+    print(signal)
+
     for x in signal:
         signal[signal.index(x)] = "".join(sorted(x))
 
     for x in output:
         output[output.index(x)] = "".join(sorted(x))
+
+    print(signal)
 
     for x in signal:
         if(len(x)) == 2:
@@ -31,8 +39,14 @@ for entry in arr:
         elif(len(x)) == 7:
             decoder[8] = x
 
+
+    print()
+    print("pop 1,4,7,8")
     for x in [1,4,7,8]:
         signal.pop(signal.index(decoder[x]))
+        print(signal)
+
+    print()
 
     for x in signal:
         if len(x) == 6:
@@ -42,37 +56,55 @@ for entry in arr:
                 else:
                     decoder[0] = x
 
+    print("pop 0 and 9")
     for x in [0,9]:
         signal.pop(signal.index(decoder[x]))
+        print(signal)
 
+    print()
+    print("pop 6")
     for x in signal:
         if len(x) == 6:
             decoder[6] = x
             signal.pop(signal.index(x))
+            print(signal)
 
     #Now only the 5 line digits are left, 2,3,5
     for x in signal:
         if decoder[1][0] in x and decoder[1][1] in x: #only 3 contains all lines that make up 1
             decoder[3] = x
             signal.pop(signal.index(x))
+    
+    print()
+    print("Find and pop 3, then 5, then last is 2")
+    print (signal)
 
     for x in signal:
         if x[0] in decoder[6] and x[1] in decoder[6] and x[2] in decoder[6] and x[3] in decoder[6] and x[4] in decoder[6]: 
             # if this signal's lines are all in the lines of 6, this must be 5, not 2
             decoder[5] = x
             signal.pop(signal.index(x))
-
+    print(signal)
     decoder[2] = signal[0]
 
+    print("letter codes for 0-9")
+    print(decoder)
+
+    print(output)
     for digit in output:
         for x in decoder:
             if digit == x:
                 output[output.index(digit)] = decoder.index(x)
 
+
     num = 0
     for x in range(4):
         num += int(output[x]) * ( 10 ** (3-x))
 
+    print(output)
+    print(num)
+
     total += num
 
+print()
 print(total)
