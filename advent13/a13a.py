9bd67d48ed277b1897f0d035b35c8cbb82445eae
Ivan <ivanyu327@gmail.com>
2021-12-14 00:12:49 -0500

    13 part 1 test

--- advent13/a13a.py
+++ advent13/a13a.py
@@ -1,6 +1,53 @@
-grid = [ [ "" for i in range(2000) ] for j in range(2000) ]
+from os import get_inheritable
+import numpy as np
 
-with open('advent12/info.txt') as f:
+coords = []
+instructions = []
+
+with open('advent13/test.txt') as f:
     for readline in f:
-        if readline.strip() != ""
-        grid.append(readline.strip().split("-"))
+        if readline.strip()!="" and readline.strip()[0] != "f":
+            temp = readline.strip().split(",")
+            coords.append([int(temp[0]),int(temp[1])])
+        elif readline.strip()!= "":
+            instructions.append(readline.strip().replace("fold along ",""))
+
+def display(grid):
+    for line in grid:
+        for point in line:
+            print(point+" ",end="")
+        print()
+
+size = np.amax(coords)+1
+
+grid = [ [ "." for i in range(size) ] for j in range(size) ]
+
+for coord in coords:
+    print(coord)
+    grid[coord[1]][coord[0]] = "#"
+
+
+display(grid)
+print(instructions)
+
+for instruction in instructions:
+    line = int(instruction[2:])
+
+    if instruction[0] == "y":
+        for r in range(line,len(grid)):
+            for c in range(len(grid[r])):
+                if grid[r][c] == "#":
+                    grid [r][c] = "."
+                    grid [line-(r-line)][c] = "#"
+    else:
+        for r in range(len(grid)):
+            for c in range(line,len(grid[r])):
+                if grid[r][c] == "#":
+                    grid [r][c] = "."
+                    grid [r][line-(c-line)] = "#"
+    
+    print(instruction)
+
+    display(grid)
+
+print(np.sum(np.char.count(grid, sub ='#')))
