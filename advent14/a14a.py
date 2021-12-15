8d82af28d65fc398a26cfaf5d3c07366a2dd94fb
Ivan <ivanyu327@gmail.com>
2021-12-15 11:03:01 -0500

    cleaned up code

--- advent14/a14a.py
+++ advent14/a14a.py
@@ -1,35 +1,41 @@
 from collections import Counter
-polymer = ""
-templates = []
 
-# file = "advent14/test.txt"
-file = "advent14/info.txt"
+def readFile():
+    file = "advent14/test.txt"
+    # file = "advent14/info.txt"
 
-with open(file) as f:
-    for readline in f:
-        if polymer == "":
-            polymer = readline.strip()
-        
-        elif readline.strip() != "":
-            temp = readline.strip().split(" -> ")
-            templates.append([temp[0],temp[1]])
+    polymer = ""
+    templates=[]
 
-print(polymer)
-print(templates)
-
-for step in range (1,11):
-    for x in range(len(polymer)-2,-1,-1):
-        for template in templates:
-            if polymer[x]+polymer[x+1] == template[0]: 
-                polymer = polymer[:x+1]+template[1]+polymer[x+1:]
-                break
+    with open(file) as f:
+        for readline in f:
+            if polymer == "":
+                polymer = readline.strip()
+            
+            elif readline.strip() != "":
+                temp = readline.strip().split(" -> ")
+                templates.append([temp[0],temp[1]])
     
-    print(step)
+    return polymer,templates
 
-most = Counter(polymer).most_common()[0]
+def solve(polymer,templates):
+    for step in range (1,11):
+        for x in range(len(polymer)-2,-1,-1):
+            for template in templates:
+                if polymer[x]+polymer[x+1] == template[0]: 
+                    polymer = polymer[:x+1]+template[1]+polymer[x+1:]
+                    break
+        
+        print(step)
 
-least = Counter(polymer).most_common()[:-2:-1] 
+    most = Counter(polymer).most_common()[0]
+    least = Counter(polymer).most_common()[:-2:-1] 
 
-print(most)
-print(least)
-print(most[1]-least[0][1])
+    print(most)
+    print(least)
+    print(most[1]-least[0][1])
+
+if __name__ == "__main__":
+    polymer = readFile()[0]
+    templates = readFile()[1]
+    solve(polymer,templates)
