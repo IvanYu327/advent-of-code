97cb6b62c273f03c537269ace05e443da44ef31c
Ivan <ivanyu327@gmail.com>
2022-12-01 15:52:10 -0500

    AOC day 1 both parts solutions

---
+++ 2022/advent01/a1.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+arr = []
+
+with open('2022/advent01/info.txt', "r") as f:
+    for readline in f: 
+        arr.append(readline.strip())
+
+elfTotals =  []
+currentElfSum = 0
+
+for calorie in arr:
+    
+    if calorie != '':
+        currentElfSum += int(calorie)
+    else:
+        elfTotals.append(currentElfSum)
+        currentElfSum = 0
+
+print(np.max(elfTotals))
+
+elfTotals.sort()
+
+print(np.sum(elfTotals[-3:]))
