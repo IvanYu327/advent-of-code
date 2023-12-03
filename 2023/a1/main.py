305c4ab427066431896e121e842d277763230c2e
Ivan <ivanyu327@gmail.com>
2023-12-03 18:59:49 -0500

    day 1 part 1

--- 2023/a1/main.py
+++ 2023/a1/main.py
@@ -3,13 +3,27 @@ import re
 
 arr = []
 
-with open('2022/advent/input.txt', "r") as f:
+with open('2023/a1/input.txt', "r") as f:
     for readline in f: 
-        line = re.split(r",| ", readline.strip())
+        line = re.sub(r'\D', '', readline.strip())
         # line = list(map(int, line))
         arr.append(line)
 
 # PART 1
+
+total = 0
+
+for line in arr:
+    if len(line) == 0:
+        continue
+    if len(line) == 1:
+        total += int(line + line)
+        continue
+    num = line[0] + line[-1]
+
+    print(line, num)
+    total += int(num)
+print(total)
 
 
 
