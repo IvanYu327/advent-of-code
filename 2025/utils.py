0ad3d3f166fb79918f25631bd95e7885add206f1
Ivan Yu <ivanyu327@gmail.com>
2025-12-01 23:28:46 -0500

    rename

--- 2025/utils.py
+++ 2025/utils.py
@@ -1,6 +1,6 @@
 def get_input(day):
     arr = []
-    with open(f'2024/input{day}.txt', "r") as f:
+    with open(f'2025/input{day}.txt', "r") as f:
         for readline in f:         
             arr.append(readline.strip())
 
